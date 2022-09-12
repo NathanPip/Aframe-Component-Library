370a0d87dfe075960ba5d70c0399c8c4e0af2650
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-09-12 16:36:35 -0700

    start of loading screen

--- components/loading-manager/loading-manager.js
+++ components/loading-manager/loading-manager.js
@@ -6,6 +6,43 @@ function delay(milisec) {
   });
 }
 
+AFRAME.registerSystem('load-screen', {
+  schema: {
+    
+  },
+
+  init: function () {
+    this.loaderScene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.0005, 10000);
+    render = function () {
+      this.el.renderer.render(this.loaderScene, this.camera);
+    };
+  
+    this.loaderScene.background = new THREE.Color(backgroundColor);
+    this.loaderScene.add(this.camera);
+    const sceneEl = this.el;
+    // Delay 200ms to avoid loader flashes.
+    setTimeout(function () {
+      if (sceneEl.hasLoaded) { return; }
+      this.resize();
+      window.addEventListener('resize', this.resize());
+      sceneEl.renderer.setAnimationLoop(render);
+    }, 200);
+  },
+  resize: function () {
+    
+  },
+
+  remove: function () {
+    // Do something the component or its entity is detached.
+  },
+
+  tick: function (time, timeDelta) {
+    // Do something on every scene tick or frame.
+  }
+});
+
+
 AFRAME.registerComponent("loading-manager", {
   schema: {
     loadSelf: { default: true },
