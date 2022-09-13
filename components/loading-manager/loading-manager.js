2ce5d0972359a46683735c79766a6ee0175122ec
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-09-13 13:54:15 -0700

    render loading screen as seperate scene

--- components/loading-manager/loading-manager.js
+++ components/loading-manager/loading-manager.js
@@ -6,42 +6,71 @@ function delay(milisec) {
   });
 }
 
-AFRAME.registerSystem('load-screen', {
-  schema: {
-    
-  },
-
-  init: function () {
+class LoadScreen {
+  constructor(enabled) {
+    this.enabled = enabled;
     this.loaderScene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.0005, 10000);
-    render = function () {
-      this.el.renderer.render(this.loaderScene, this.camera);
-    };
-  
-    this.loaderScene.background = new THREE.Color(backgroundColor);
-    this.loaderScene.add(this.camera);
-    const sceneEl = this.el;
-    // Delay 200ms to avoid loader flashes.
-    setTimeout(function () {
-      if (sceneEl.hasLoaded) { return; }
-      this.resize();
-      window.addEventListener('resize', this.resize());
-      sceneEl.renderer.setAnimationLoop(render);
-    }, 200);
-  },
-  resize: function () {
-    
-  },
-
-  remove: function () {
-    // Do something the component or its entity is detached.
-  },
-
-  tick: function (time, timeDelta) {
-    // Do something on every scene tick or frame.
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.clock = new THREE.Clock();
+    this.init();
   }
-});
 
+  init() {
+    document.querySelector("a-scene").style.display = "none";
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    console.log("called")
+    this.renderer.domElement.classList.add("loading-scene");
+    this.renderer.domElement.style.zIndex = -1;
+    this.renderer.domElement.style.position = "fixed";
+    this.renderer.domElement.style.top = 0;
+    this.renderer.domElement.style.left = 0;
+    document.body.appendChild(this.renderer.domElement);
+    this.loaderScene.add(this.camera);
+    this.loaderScene.background = new THREE.Color(0x000000);
+    
+    const ambient = new THREE.AmbientLight(0xffffff, .5);
+    this.loaderScene.add(ambient);
+
+    const pointLight = new THREE.PointLight(0xfffff, 5, 100);
+    pointLight.position.set(-20, 10, -50)
+    this.loaderScene.add(pointLight);
+
+    const pointLight2 = new THREE.PointLight(0x0000ff, 5, 100);
+    pointLight2.position.set(0, 10, -70)
+    this.loaderScene.add(pointLight2);
+
+    const pointLight1 = new THREE.PointLight(0xffffff, 5, 100);
+    pointLight1.position.set(20, 10, -50)
+    this.loaderScene.add(pointLight1);
+
+
+    const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+    const material = new THREE.MeshStandardMaterial( { color: 0x5FEAF4, roughness: .15, metalness: 1 } );
+    this.torusKnot = new THREE.Mesh( geometry, material );
+
+    this.loaderScene.add(this.torusKnot );
+    this.torusKnot.position.z = -60;
+
+    this.render();
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  render() {
+    requestAnimationFrame(() => {
+      let time = this.clock.getElapsedTime();
+      this.torusKnot.rotation.y = .5 * time;
+      this.renderer.render(this.loaderScene, this.camera);
+      this.render();
+    })
+  }
+}
 
 AFRAME.registerComponent("loading-manager", {
   schema: {
